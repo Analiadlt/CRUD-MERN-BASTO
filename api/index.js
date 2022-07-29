f82b7526795f08b8d0216df7b7f93f02b75a419d
Analiadlt <87041305+Analiadlt@users.noreply.github.com>
2022-07-29 15:34:22 -0300

    heroku config

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 //Main
 const server = require('./src/app.js');
+const port = process.env.API_PORT || 3001;
 
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log('%s listening at 3001'); 
   });
   
--- api/src/db.js
+++ api/src/db.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 (async () => {
   try {      
-    const db = await mongoose.connect("mongodb://localhost/basto");
+    const db = await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/basto");
     console.log('DB connected to', db.connection.name);
   } catch (err) {
     console.error(err);
